7daaec031926fd3cf76c233c0a805d885017ca35
Maria Richards <53357020+aTechNewbie38@users.noreply.github.com>
2019-09-12 20:30:33 -0400

    Completed the JS-II MVP all 3 files: Callbacks.js, Closures.js and Array-methods.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -4,6 +4,12 @@
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
 
+var me = 'Maria Richards'
+function greetMe(){
+  console.log('Hello, ' + me + '!')
+}
+greetMe()
+
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
